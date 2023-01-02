210d7a6d99eb8f9192af8e7b6886b0f463df3edd
zmf17 <57649230+zmf17@users.noreply.github.com>
2023-01-02 17:34:33 +0800

    Update Basic_Adder.v

--- 加法器/Basic_Adder.v
+++ 加法器/Basic_Adder.v
@@ -1,1 +1,10 @@
+module FA(input a, input b, input cin, output sum, output cout);
+	assign {cout,sum} = a+b+cin;
+endmodule
+
+module HA(input a, input b, output sum, output cout);
+	assign {cout,sum} = a+b;
+endmodule
+
+
 
