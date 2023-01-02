56bd464cfa3eeb934245dc170d9f5a30a2a9426c
zmf17 <57649230+zmf17@users.noreply.github.com>
2023-01-02 17:45:22 +0800

    Update Basic_Adder.v

--- 加法器/Basic_Adder.v
+++ 加法器/Basic_Adder.v
@@ -6,5 +6,12 @@ module HA(input a, input b, output sum, output cout);
 	assign {cout,sum} = a+b;
 endmodule
 
+module Carry_generate(input a, input b, output g);
+	assign g = a&b;
+endmodule 
+
+module Carry_propagate(input a, input b, output g);
+	assign p = a^b; 
+endmodule 
 
 
